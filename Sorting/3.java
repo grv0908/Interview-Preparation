dfe8f70a36a9c04144816a07d97ab341d9a81af2
Gaurav Rajput <grv0908@gmail.com>
2017-10-19 00:17:52 +0530

    InsertionSort

--- Sorting/3.java
+++ Sorting/3.java
@@ -6,6 +6,8 @@ Avg Case   : O(n^2)
 Worst Case : O(n^2)
 Stable Sort
 Inplace Algorithm
+Insertion sort takes maximum time to sort if elements are sorted in reverse order.
+And it takes minimum time (Order of n) when elements are already sorted.
 */
 
 public class InsertionSort {
